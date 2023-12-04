60e3d5f3caf2fd1571d005fa2b24b5a8115401f7
Juan Leandro Leites Barrios <laloleites@gmail.com>
2023-12-04 21:28:00 +0100

    Initial commit

---
+++ src/day_01/main.rs
@@ -0,0 +1,62 @@
+use std::fs::read_to_string;
+use std::io::{self};
+use std::num::ParseIntError;
+
+fn main() -> io::Result<()> {
+    // Test get_digits
+    let result_3 = get_digits("pqr3st4u8vwx");
+    let expected_3 = "38";
+    assert_eq!(result_3.unwrap(), expected_3);
+
+    let result_1 = get_digits("treb7uchet");
+    let expected_1 = "77";
+    assert_eq!(result_1.unwrap(), expected_1);
+
+    // Test process_file
+    let actual = process_file()?;
+    assert_eq!(actual, 142);
+
+    Ok(())
+}
+
+fn get_digits(line: &str) -> Option<String> {
+    let mut result = String::new();
+
+    for ch in line.chars() {
+        if ch.is_digit(10) {
+            result.push(ch);
+        }
+    }
+
+    if result.is_empty() {
+        return None;
+    }
+
+    if result.len() > 1 {
+        let first = result.chars().next().unwrap();
+        let last = result.chars().last().unwrap();
+        result = format!("{}{}", first, last);
+    } else {
+        result = result.repeat(2);
+    }
+
+    Some(result)
+}
+
+fn process_line(line: &str) -> Result<i32, ParseIntError> {
+    let digits = get_digits(line);
+    let value = digits.unwrap().parse();
+    value
+}
+
+fn process_file() -> io::Result<i32> {
+    let file_path = "src/day_01/01_input.txt";
+    let file = read_to_string(file_path)?;
+
+    let mut sum = 0;
+    for line in file.lines() {
+        sum += process_line(line).unwrap();
+    }
+
+    Ok(sum)
+}
---
+++ src/day_02/main.rs
@@ -0,0 +1,111 @@
+use std::fs::read_to_string;
+use std::io::{self};
+
+#[derive(PartialEq, Debug)]
+struct Throw {
+    red: i32,
+    blue: i32,
+    green: i32,
+}
+
+fn parse_throw(line: &str) -> Throw {
+    let mut red = 0;
+    let mut blue = 0;
+    let mut green = 0;
+
+    for color in line.split(",") {
+        let mut parts = color.trim().split(" ");
+        let digit = parts.next().unwrap();
+        let col = parts.next().unwrap();
+        match col {
+            "red" => red = digit.parse().unwrap(),
+            "blue" => blue = digit.parse().unwrap(),
+            "green" => green = digit.parse().unwrap(),
+            _ => (),
+        }
+    }
+
+    Throw { red, blue, green }
+}
+
+fn main() -> io::Result<()> {
+    let result: Throw = parse_throw("1 red, 2 green, 6 blue");
+    let expected = Throw {
+        red: 1,
+        blue: 6,
+        green: 2,
+    };
+    assert_eq!(result, expected);
+
+    let result: Throw = parse_throw("3 green, 10 blue");
+    let expected = Throw {
+        red: 0,
+        blue: 10,
+        green: 3,
+    };
+    assert_eq!(result, expected);
+
+    let result: Throw = parse_throw("2 green");
+    let expected = Throw {
+        red: 0,
+        blue: 0,
+        green: 2,
+    };
+    assert_eq!(result, expected);
+
+    let result = parse_line("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+    let expected_throws: Vec<Throw> = Vec::from([
+        Throw {
+            red: 4,
+            blue: 3,
+            green: 0,
+        },
+        Throw {
+            red: 1,
+            blue: 6,
+            green: 2,
+        },
+        Throw {
+            red: 0,
+            blue: 0,
+            green: 2,
+        },
+    ]);
+    assert_eq!(result, (1, expected_throws));
+
+    assert_eq!(process_file().unwrap(), 7);
+
+    Ok(())
+}
+
+fn parse_line(line: &str) -> (i32, Vec<Throw>) {
+    let mut parts = line.split(":");
+    let id_part = parts.next().unwrap();
+    let throws_part = parts.next().unwrap();
+    let throws = throws_part.split(";").map(parse_throw).collect();
+    (id_part.split(" ").nth(1).unwrap().parse().unwrap(), throws)
+}
+
+fn process_file() -> io::Result<i32> {
+    let file_path = "src/day_02/input.txt";
+    let file = read_to_string(file_path)?;
+
+    let max_game = Throw {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+
+    let mut sum = 0;
+    for line in file.lines() {
+        let (id, throws) = parse_line(line);
+        let is_any_throw_bigger = throws.iter().any(|throw| {
+            throw.red > max_game.red || throw.blue > max_game.blue || throw.green > max_game.green
+        });
+        if is_any_throw_bigger {
+            sum += id;
+        }
+    }
+
+    Ok(sum)
+}
