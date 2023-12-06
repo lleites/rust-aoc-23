b0fa77059777d4e6c3c75daebfcf37cbf6d53254
Juan Leandro Leites Barrios <laloleites@gmail.com>
2023-12-06 20:22:02 +0100

    Added functional solutions

--- src/day_04/mod.rs
+++ src/day_04/mod.rs
@@ -1,6 +1,28 @@
 use crate::utils::read_lines;
 use std::io;
 
+fn parse_card_func(line: &str) -> i32 {
+    let number_parts: Vec<&str> = line.split(":").last().unwrap().split("|").collect();
+    let winning_numbers = number_parts.first().unwrap();
+    let chances = number_parts.last().unwrap();
+
+    let winning_numbers: Vec<i32> = winning_numbers
+        .split_whitespace()
+        .flat_map(str::parse)
+        .collect();
+    let chances: Vec<i32> = chances.split_whitespace().flat_map(str::parse).collect();
+
+    let wins = chances.iter().flat_map(|chance| {
+        if winning_numbers.contains(&chance) {
+            Some(1)
+        } else {
+            None
+        }
+    });
+
+    wins.fold(0.5, |acc, _current| acc * 2.0) as i32
+}
+
 fn parse_card(line: &str) -> i32 {
     let number_parts: Vec<&str> = line.split(":").last().unwrap().split("|").collect();
     let winning_numbers = number_parts.first().unwrap();
@@ -12,26 +34,22 @@ fn parse_card(line: &str) -> i32 {
         .collect();
     let chances: Vec<i32> = chances.split_whitespace().flat_map(str::parse).collect();
 
-    let mut total = 0;
-
-    for chance in chances {
+    let wins = chances.iter().flat_map(|chance| {
         if winning_numbers.contains(&chance) {
-            if total == 0 {
-                total = 1
-            } else {
-                total = total * 2;
-            }
+            Some(1)
+        } else {
+            None
         }
-    }
+    });
 
-    total
+    wins.fold(0.5, |acc, _current| acc * 2.0) as i32
 }
 
 pub fn process_file() -> io::Result<i32> {
-    let mut result: i32 = 0;
-    for line in read_lines("src/day_04/input.txt") {
-        result += parse_card(&line);
-    }
+    let result: i32 = read_lines("src/day_04/input.txt")
+        .iter()
+        .map(|x| parse_card(&x))
+        .sum();
 
     Ok(result)
 }
@@ -46,8 +64,15 @@ mod tests {
 
         assert_eq!(result, 8);
     }
-    #[test]
 
+    #[test]
+    fn test_parse_card_func() {
+        let result = parse_card("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
+
+        assert_eq!(result, 8);
+    }
+
+    #[test]
     fn test_process_file() {
         let result = process_file().unwrap();
 
